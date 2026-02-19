49e7ea85857d0ba7a96a89759e2d79c64128da4a
shoorya <shauryamandawariya@gmail.com>
2026-02-19 12:26:16 +0530

    Time: 2 ms (77.67%) | Memory: 13.5 MB (17.20%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int i = 0;
+        int j = n;
+        vector<int> ans;
+
+        while (j < nums.size()) {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++;
+            j++;
+        }
+
+        return ans;
+    }
+};
