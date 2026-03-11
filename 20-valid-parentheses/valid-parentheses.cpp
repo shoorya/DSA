f9014017358818b21406ce97f01817e81ccf401e
shoorya <shauryamandawariya@gmail.com>
2026-03-11 11:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -4,21 +4,21 @@ public:
 
         stack<char> st;
 
-        for(char c:s){
-            if(c=='('||c=='{'||c=='[')
-                st.push(c);
-            else{
-                if(st.empty()) return false;
+        for (auto x : s) {
+            if (x == '(' || x == '{' || x == '[')
+                st.push(x);
+            else {
+                if (st.empty()) return false;
 
-                char t=st.top();
+                auto t = st.top();
                 st.pop();
 
-                if((c==')'&&t!='(')||
-                   (c=='}'&&t!='{')||
-                   (c==']'&&t!='['))
+                if ((x == ')' && t != '(') ||
+                    (x == '}' && t != '{') ||
+                    (x == ']' && t != '['))
                     return false;
             }
         }
         return st.empty();
     }
-};;
+};
